3db4e3d5f8a7c2138e138d7bc8ff18b9dd5d17ea
rohan4191 <rohan.parekh419@gmail.com>
2024-12-14 15:56:12 -0800

    start on guestbook

---
+++ makeGuestbook.js
@@ -0,0 +1,27 @@
+const comments = [
+  { name: "", time: "", body: "" },
+  {
+    name: "xxZANE_ROCKS_13xx",
+    time: "2 years ago",
+    body: "youre webapge stinks",
+  },
+  { name: "alice", time: "9 months ago", body: "haiiii :3" },
+];
+const commentContainer = document.getElementById("container");
+comments.forEach(({ name, time, body }) => {
+  const comment = document.createElement("div");
+  comment.classList = "comment";
+
+  const header = document.createElement("p");
+  header.classList = "commentHeader";
+  const message = document.createElement("p");
+  message.classList = "commentBody";
+
+  header.innerHTML = name + " | " + time;
+  message.innerHTML = body;
+
+  comment.appendChild(header);
+  comment.appendChild(message);
+
+  commentContainer.appendChild(comment);
+});
