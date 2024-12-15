6187b43acec0378b4ec4b9db656cc5b98c51b364
rohan4191 <rohan.parekh419@gmail.com>
2024-12-14 18:37:21 -0800

    start on status history, julia guestbook, collapsed styles

--- scripts/makeGuestbook.js
+++ scripts/makeGuestbook.js
@@ -5,7 +5,62 @@ const comments = [
     time: "2 years ago",
     body: "youre webapge stinks",
   },
+  {
+    name: "g3th@ck3d!",
+    time: "1 year ago",
+    body: "you have been visited by the NEOCITY NIBBLER. your page looks nice and scrumptious. share this on 13 other pages or else your neocity will get nibbled",
+  },
   { name: "alice", time: "9 months ago", body: "haiiii :3" },
+  {
+    name: "meowZA",
+    time: "5 months ago",
+    body: "hello bloggger! youve earned one of my rarest meows. enjoy uwu!\n\
+    ／＞　 フ\n\
+  | 　_　_| \n\
+／` ミ＿xノ \n\
+/　　　　 |\n\
+/　 ヽ　　 ﾉ\n\
+│　　|　|　|\n\
+／￣|　　 |　|　|\n\
+(￣ヽ＿_ヽ_)__)\n\
+＼二)ℒ𝓸𝓿𝒆 𝔂𝓸𝓾 ",
+  },
+
+  {
+    name: "cait4life",
+    time: "3 months ago",
+    body: "smh not another jinx fan. basic.",
+  },
+  {
+    name: "normalOrmal",
+    time: "2 months ago",
+    body: "aaaaaaa luv ur page. rain animation is so cute i could drink it up. hearts so sparkly they hurt my eyes. flawless. devine. byootiful.",
+  },
+  {
+    name: "MAINUSEENAMEHEREEIDK",
+    time: "2 months ago",
+    body: "@normalOrmal. it is not supposed to be cute. it's supposed to be sad and dreery. bc thats how i feel.",
+  },
+  {
+    name: "normalOrmal",
+    time: "2 months ago",
+    body: "@mainusernam,ehereidk oh, oki.",
+  },
+  {
+    name: "ellabella",
+    time: "1 month ago",
+    body: "O i love your landing page!! but what happened :o your pages are so cute i wish you still posted",
+  },
+  {
+    name: "spyGuy35",
+    time: "2 weeks ago",
+    body: "you're not the only one who lost a lot. just sayin.",
+  },
+  {
+    name: "alice",
+    time: "1 day ago",
+    body: "you have been visited by the NEOCITY NIBBLER. your page looks nice and scrumptious. share this on 13 other pages or else your neocity will get nibbled",
+  },
 ];
 const commentContainer = document.getElementById("container");
 comments.forEach(({ name, time, body }) => {
---
+++ scripts/makeStatuses.js
@@ -0,0 +1,27 @@
+const USERNAME = "bean";
+
+const statuses = [
+  {
+    time: "2 years ago",
+    body: "crying weeping sobbing",
+  },
+  { time: "9 months ago", body: "hapy" },
+];
+const statusContainer = document.getElementById("entry-list");
+statuses.forEach(({ time, body }) => {
+  const status = document.createElement("div");
+  status.className = "entry";
+
+  const header = document.createElement("p");
+  header.classList = "entry-header";
+  const message = document.createElement("p");
+  message.classList = "entry-content";
+
+  header.innerHTML = USERNAME + " | " + time;
+  message.innerHTML = body;
+
+  status.appendChild(header);
+  status.appendChild(message);
+
+  statusContainer.appendChild(status);
+});
