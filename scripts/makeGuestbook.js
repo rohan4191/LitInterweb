37779e86578802f59fabbf5863c7c42c8934cc02
rohan4191 <rohan.parekh419@gmail.com>
2024-12-16 19:35:45 -0800

    cursor effects, more guestbook, some status hist

--- scripts/makeGuestbook.js
+++ scripts/makeGuestbook.js
@@ -3,9 +3,24 @@ const BFF = "<span style='color: red'>m_alice</span>";
 
 let comments = [
   {
-    name: "xxZANE_ROCKS_13xx",
+    name: "lux",
     time: "3 years ago",
-    body: "youre webapge stinks",
+    body: "first",
+  },
+  {
+    name: USER,
+    time: "3 years ago",
+    body: "@lux lolll thanks for being the first comment on my guestbook. is there anything else you wanted to say? maybe i should add some questions to this page in case people dk what to comment",
+  },
+  {
+    name: "strwbwnny",
+    time: "3 years ago",
+    body: "your site is super cute ^_^ can't wait to see how it grows!",
+  },
+  {
+    name: USER,
+    time: "3 years ago",
+    body: "@strwbwnny thanks!!! i hope you stick around cuz im planning even more awesome stuff in the future!!!",
   },
   {
     name: "g3th@ck3d!",
@@ -15,25 +30,25 @@ let comments = [
 
   {
     name: BFF,
-    time: "1 year ago",
+    time: "2 years ago",
     body: "Awesome site!! I also love MM!!",
   },
   {
     name: USER,
-    time: "1 year ago",
+    time: "2 years ago",
     body: "@" + BFF + " madoka magica or mystic messenger lol",
   },
   {
     name: BFF,
-    time: "1 year ago",
+    time: "2 years ago",
     body:
       "@" +
       USER +
-      " Oh HAHA. I meant Mystic Messenger. I'm super in love with Seven. Ur kinda like him you know? Like a cool hacker type. Just sayin!",
+      " Oh HAHA. I meant Mystic Messenger. I'm super in love with Seven. Ur kinda like him you know? A cool hacker type. Like how did you make the cursor do that?? Just sayin!",
   },
   {
     name: USER,
-    time: "1 year ago",
+    time: "2 years ago",
     body:
       "@" +
       BFF +
@@ -41,17 +56,17 @@ let comments = [
   },
   {
     name: USER,
-    time: "1 year ago",
+    time: "2 years ago",
     body: "@" + BFF + " (wink wink you should sub)",
   },
   {
     name: USER,
-    time: "1 year ago",
+    time: "2 years ago",
     body: "@" + BFF + " I mean only if you want to!!!!",
   },
   {
     name: BFF,
-    time: "1 year ago",
+    time: "2 years ago",
     body:
       "@" +
       USER +
@@ -59,7 +74,7 @@ let comments = [
   },
   {
     name: USER,
-    time: "1 year ago",
+    time: "2 years ago",
     body:
       "@" +
       BFF +
@@ -68,66 +83,66 @@ let comments = [
 
   {
     name: BFF,
-    time: "1 year ago",
+    time: "2 years ago",
     body:
       "@" +
       USER +
-      " You're the best! Heres a <a href='alice.html'>link</a> to my page if you wanna check it out : )\nI also have my msngr there if you wanna chat more : )",
+      " You're the best! Heres a <a href='alice.html'>link</a> to my page if you wanna check it out : )\nI also have a chatbox there if you wanna talk more : )",
   },
 
-  { name: USER, time: "1 year ago", body: "@" + BFF + " will do :}" },
+  { name: USER, time: "2 years ago", body: "@" + BFF + " will do :}" },
 
   {
     name: "berry",
-    time: "1 year ago",
+    time: "2 years ago",
     body: "sick as hell website. this shit rocks",
   },
 
-  { name: USER, time: "1 year ago", body: "hello and welcome and tysm!!!!" },
+  { name: USER, time: "2 years ago", body: "hello and welcome and tysm!!!!" },
 
   {
     name: "Teef",
-    time: "1 year ago",
+    time: "2 years ago",
     body: "Wow. I think this is one of my favorite sites on the web.",
   },
 
   {
     name: USER,
-    time: "1 year ago",
+    time: "2 years ago",
     body: "@Teef omgggg >w< thats super nice of you!!! do you wanna be on my friends page???",
   },
 
   {
     name: "Teef",
-    time: "1 year ago",
+    time: "2 years ago",
     body: "Oh wow. I mean, sure!",
   },
 
   {
     name: "miauau",
-    time: "1 year ago",
+    time: "2 years ago",
     body: "I LOVE YOURE AESTHETIC SOSOSOSO MUICH SKSKSKKSKSKSK",
   },
 
   {
     name: USER,
-    time: "1 year ago",
+    time: "2 years ago",
     body: "@miauau YESSS TYYYY im soso glad you appreciate :} anything you like specifically??? im happy to answer any questions about how i did the layout and things if you want!!!!",
   },
 
   {
     name: "alex",
-    time: "1 year ago",
+    time: "2 years ago",
     body: "@miauau *your...",
   },
   {
     name: USER,
-    time: "1 year ago",
+    time: "2 years ago",
     body: "@alex tysm for the comment!!! thanks for visiting the page!!",
   },
   {
     name: "sarahpie",
-    time: "1 year ago",
+    time: "2 years ago",
     body:
       "I checked this out from the link from " +
       BFF +
@@ -135,17 +150,17 @@ let comments = [
   },
   {
     name: USER,
-    time: "1 year ago",
+    time: "2 years ago",
     body:
       "ohh are you all here from " + BFF + "??? thats really cool thank you",
   },
   {
     type: "meta",
-    body: "---------------------|216 comments hidden|---------------------",
+    body: "---------------------|116 comments hidden|---------------------",
   },
   {
     name: "meowZA",
-    time: "11 months ago",
+    time: "1 year ago",
     body: "hello webmaster! youve earned one of my rarest meows. enjoy uwu!\n\
     ／＞　 フ\n\
   | 　_　_| \n\
@@ -160,13 +175,13 @@ let comments = [
 
   {
     name: "aaaaati",
-    time: "10 months ago",
+    time: "1 year ago",
     body: "this site is pretty baller. what do i have to do to get on the friends page?",
   },
 
   {
     name: USER,
-    time: "10 months ago",
+    time: "1 year ago",
     body:
       "@" +
       "aaaaati" +
@@ -174,12 +189,12 @@ let comments = [
   },
   {
     name: "aaaaati",
-    time: "10 months ago",
+    time: "1 year ago",
     body: "well i guess im a gemini",
   },
   {
     name: USER,
-    time: "10 months ago",
+    time: "1 year ago",
     body:
       "@" +
       "aaaaati" +
@@ -188,13 +203,13 @@ let comments = [
 
   {
     name: BFF,
-    time: "10 months ago",
+    time: "1 year ago",
     body: "So not to put you on blast in public but when am I getting on that friends page?? Haven't I earned it by now?? /j",
   },
 
   {
     name: USER,
-    time: "10 months ago",
+    time: "1 year ago",
     body:
       "@" +
       BFF +
@@ -203,37 +218,100 @@ let comments = [
 
   {
     name: BFF,
-    time: "10 months ago",
+    time: "1 year ago",
     body: "NO WAY?? MY OWN NAME COLOR??",
   },
 
   {
     name: USER,
-    time: "10 months ago",
+    time: "1 year ago",
     body: "haha yeah it was no big deal, actually pretty easy",
   },
   {
     name: BFF,
-    time: "10 months ago",
+    time: "1 year ago",
     body: "Well I am honored. It's really cool. Not as cool as the animated name you've given yourself though lol!!",
   },
   {
     name: USER,
-    time: "10 months ago",
+    time: "1 year ago",
     body: "thx hahaha i have to keep something special for myself so everyone notices when im commenting",
   },
 
   {
     name: BFF,
-    time: "8 months ago",
-    body: "silly inside joke",
+    time: "11 months ago",
+    body: "EVERYONE. This is an important annoucement. It is Cubey's SITE BIRTHDAY!! Be sure to leave a comment, or invoke my wrath!!",
   },
   {
     name: USER,
-    time: "8 months ago",
-    body: "response showing deeper level of friendship",
+    time: "11 months ago",
+    body: "awawawawaa aliceee you didnt have to do thatttt",
+  },
+  {
+    name: BFF,
+    time: "11 months ago",
+    body: "Shushhhhh. My site wouldn't have nearly as many visitors if it wasn't for your genius coding, Seven :)",
   },
 
+  {
+    name: "miauau",
+    time: "11 months ago",
+    body: "HAPPY NEOBIRTHDAY CUBEY",
+  },
+  {
+    name: "sarahpie",
+    time: "11 months ago",
+    body: "Happy site bday. LOVE the most recent update",
+  },
+  {
+    type: "meta",
+    body: "---------------------|53 comments hidden|---------------------",
+  },
+  {
+    name: USER,
+    time: "11 months ago",
+    body: "guysss plsss tysm i cant even respond to everyone 😭",
+  },
+
+  {
+    name: "Teef",
+    time: "9 months ago",
+    body: "Hey. Is this site still getting updated? It's been almost a month since the last changes.",
+  },
+  {
+    name: USER,
+    time: "9 months ago",
+    body:
+      "@Teef tysm for asking!!! yeah no im spending a lot of time helping " +
+      BFF +
+      " with her site so havent been working on mine as much srryyy",
+  },
+  {
+    name: BFF,
+    time: "9 months ago",
+    body: "Sorry for stealing Cubey from you all, but it's really paying off!! We've almost cracked the top 50 sites by daily viewers, which means we'll get featured on the front page soon!!",
+  },
+  {
+    name: USER,
+    time: "9 months ago",
+    body: "haha yeah its like im kyubey from madoka magica and im giving you magic powers",
+  },
+  {
+    name: BFF,
+    time: "9 months ago",
+    body: "HAHA you're so right. I guess I'm finally going to have to watch that show now, after all you've done for me :)",
+  },
+  {
+    name: "Teef",
+    time: "9 months ago",
+    body: "But everything's good IRL? Some of your statuses make it sound like you're going through some stuff...",
+  },
+  {
+    name: USER,
+    time: "9 months ago",
+    body: "ohhh plsss dw about meeee but tyty for asking. im just focusing on alices website since thats whats most important!!!!",
+  },
   {
     name: "normalOrmal",
     time: "1 month ago",
--- scripts/makeStatuses.js
+++ scripts/makeStatuses.js
@@ -1,9 +1,29 @@
-const USERNAME = "cyubey";
+const USER = "<span class=username>cyubey</span>";
 
 const statuses = [
   {
-    time: "2 years ago",
+    time: "9 months ago",
     body: "wishing that my dad would EVER KNOCK",
+  },
+  {
+    time: "3 years ago",
+    body: "",
+  },
+  {
+    time: "3 years ago",
+    body: "got a guestbook comment notification and got super excited but it was just a chainmail :{",
+  },
+  {
+    time: "3 years ago",
+    body: "if youre reading this strwbwnny thanks for commenting youre really nice :}",
+  },
+  {
+    time: "3 years ago",
+    body: "first comment on my guestbook??? ig that means people are actually visiting!!!! if ur reading this plssss leave a comment",
+  },
+  {
+    time: "3 years ago",
+    body: "super pumped about my new website!!!!! look at my awesome status box :}",
   },
 ];
 const statusContainer = document.getElementById("entry-list");
@@ -16,7 +36,7 @@ statuses.forEach(({ time, body }) => {
   const message = document.createElement("p");
   message.classList = "entry-content";
 
-  header.innerHTML = USERNAME + " | " + time;
+  header.innerHTML = time + " | " + USER;
   message.innerHTML = body;
 
   status.appendChild(header);
